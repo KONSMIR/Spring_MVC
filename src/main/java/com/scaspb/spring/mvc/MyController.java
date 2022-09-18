fd8dac8c892c7bdb36e88bc2b41e348a43f13f2e
KONSMIR <sca.spb@gmail.com>
2022-09-18 17:00:26 +0300

    Test MVC

--- src/main/java/com/scaspb/spring/mvc/MyController.java
+++ src/main/java/com/scaspb/spring/mvc/MyController.java
@@ -1,13 +1,67 @@
 package com.scaspb.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.naming.Binding;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
     public String showFirstView() {
         return "first-view";
     }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model) {
+        Employee emp = new Employee();
+        emp.setName("Enter your name");
+        emp.setSurname("Enter your surname");
+        emp.setSalary(0);
+        model.addAttribute("employee", emp);
+        return "ask-emp-details-view";
+    }
+
+    @RequestMapping("showDetails")
+    public String showEpmDetails(@Valid @ModelAttribute("employee") Employee emp,
+                                 BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-emp-details-view";
+        }
+    }
+
+//    @RequestMapping("showDetails")
+//    public String showEpmDetails() {
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("showDetails")
+//    public String showEpmDetails(HttpServletRequest request, Model model) {
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        model.addAttribute("description",
+//                " - instructor");
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("showDetails")
+//    public String showEpmDetails(@RequestParam("employeeName") String empName,
+//                                 Model model) {
+//        empName = "Mr. " + empName + "!";
+//        model.addAttribute("nameAttribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
 }
--- src/main/webapp/WEB-INF/view/first-view.jsp
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -5,6 +5,10 @@
 <body>
 
 <h2>this test Spring MVC project</h2>
+<br>
+<br>
+
+<a href="askDetails">Pleas write your details</a>
 
 </body>
 </html>
